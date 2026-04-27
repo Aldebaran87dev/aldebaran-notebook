ba1bc201b158198fe6f4d4159073c250fbf3bccf
Aldebaran <Aldebaran87dev@users.noreply.github.com>
2026-04-27 07:17:11 -0500

    set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // For GitHub Pages at https://aldebaran87.github.io/aldebaran-notebook/
-  // uncomment before deploying:
-  // base: "/aldebaran-notebook/",
-  base: "/",
+  base: "/aldebaran-notebook/",
 });
